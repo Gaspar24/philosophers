9ab15072db5fb033183bfc9437612b46b8921d7b
Marian Sacaliuc <msacaliu@c2r10s8.42wolfsburg.de>
2024-05-03 13:42:15 +0200

    comited

--- dinner.c
+++ dinner.c
@@ -6,7 +6,7 @@
 /*   By: msacaliu <msacaliu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 17:48:27 by msacaliu          #+#    #+#             */
-/*   Updated: 2024/05/03 13:22:52 by msacaliu         ###   ########.fr       */
+/*   Updated: 2024/05/03 13:41:44 by msacaliu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 	//same algo but..
 	// 1)fake to lock the fork
 	// 2) sleep until the monitor will bust it
-	
+
 	void	*lone_philo(void *arg)
 	{
 		t_philo *philo;
@@ -29,34 +29,28 @@
 			usleep(100);
 		return(NULL);
 	}
-
-
 	// THINKING routine  
 void thinking(t_philo *philo, bool pre_simulation) 
 {
 
-	(void)pre_simulation;
-	write_status(THINKING,philo);
-	// long	t_eat;
-	// long	t_sleep;
-	// long	t_think;
+	long	t_eat;
+	long	t_sleep;
+	long	t_think;
 
-	// if(!pre_simulation)	
-	// 	write_status(THINKING,philo);
-	// // if the system is even , we don t care, system already fair
-	// if (philo->data->philo_nb % 2 == 0)
-	// 	return ;
-	// // odd, not always fair
-	// t_eat = philo->data->time_to_eat;
-	// t_sleep = philo->data->time_to_sleep;
-	// t_think = (t_eat * 2) - t_sleep;
-	// if (t_think < 0)
-	// 	t_think = 0;
-	// // precise control in want to make;
-	// mod_usleep(t_think, philo->data); 
+	if(!pre_simulation)	
+		write_status(THINKING,philo);
+	// if the system is even , we don t care, system already fair
+	if (philo->data->philo_nb % 2 == 0)
+		return ;
+	// odd, not always fair
+	t_eat = philo->data->time_to_eat;
+	t_sleep = philo->data->time_to_sleep;
+	t_think = (t_eat * 2) - t_sleep;
+	if (t_think < 0)
+		t_think = 0;
+	// precise control in want to make;
+	mod_usleep(t_think, philo->data); 
 }
-
-
 
 	// EAT routine
 		// 1. grab the forks;
@@ -90,18 +84,9 @@ void	*philo_routine(void *data)
 	
 	t_philo *philo;
 	philo = (t_philo *) data;
-	//spin lock  // unitill all flags are set to true
-	// wait_all_threads(philo->data); // every philo will wait for the treads to be ready
-	
 	// set time_last_meal;
 	set_long(&philo->philo_mutex, &philo->last_meal_time, get_time(MILISECOND));
 	
-	// syncro with monitor
-	// incrise a table variable with all threads running
-	// increase_long(&philo->data->data_mutex, &philo->data->threads_running_nb);
-	
-	// desyncronizing philos
-	// de_syncronize_philos(philo);
 	while (!simulation_finished(philo->data))
 	{
 		// 1) am i full?
